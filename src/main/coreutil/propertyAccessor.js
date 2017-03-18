5e04a1b6be9bd2f44ab30526c26e1b662cefef85
Toby Nilsen <torbjorn.nilsen@gmail.com>
2017-03-18 19:26:58 +0800

    Move propertyAccessor.

---
+++ src/main/coreutil/propertyAccessor.js
@@ -0,0 +1,31 @@
+export class PropertyAccessor{
+
+    static getValue(destination, name) {
+        var pathArray = name.split('.');
+        for (var i = 0, n = pathArray.length; i < n; ++i) {
+            var key = pathArray[i];
+            if (key in destination) {
+                destination = destination[key];
+            } else {
+                return;
+            }
+        }
+        return destination;
+    }
+
+    static setValue(destination, name, value) {
+        var pathArray = name.split('.');
+        for (var i = 0, n = pathArray.length; i < n; ++i) {
+            var key = pathArray[i];
+            if(i == n-1){
+                destination[key] = value;
+                return;
+            }
+            if (!(key in destination) || destination[key] === null) {
+                destination[key] = {};
+            }
+            destination = destination[key];
+        }
+    }
+
+}
