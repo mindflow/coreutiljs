3c8d52b8069a4af1a8793fc5e1cb62fb86e14b3e
Mindflow <torbjorn.nilsen@gmail.com>
2020-09-06 00:33:08 +1000

    Time Promise.

---
+++ src/coreutil/timePromise.js
@@ -0,0 +1,18 @@
+export class TimePromise {
+
+    /**
+     * Return promise which executes the promiseFunction within given time
+     * @param {number} time 
+     * @param {Function} promiseFunction 
+     * @returns {Promise}
+     */
+    static asPromise(time, promiseFunction) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                promiseFunction.call();
+                resolve.call();
+            }, time);
+        });
+    }
+
+}
