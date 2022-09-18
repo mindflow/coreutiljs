11a9cef872ce37871639f1a8f7f3064d998133a1
Toby <torbjorn.nilsen@gmail.com>
2022-09-19 08:19:27 +1000

    StringTemplate.

---
+++ src/coreutil/stringTemplate.js
@@ -0,0 +1,29 @@
+export class StringTemplate {
+
+    /**
+     * 
+     * @param {String} theString 
+     * @returns {StringTemplate}
+     */
+    static from(theString) {
+        return new StringTemplate(theString);
+    }
+
+    /**
+     * 
+     * @param {String} theString 
+     */
+    constructor(theString) {
+        this.theString = theString;
+    }
+
+    set(key, value) {
+        this.theString = this.theString.replace("{" + key + "}", value);
+        return this;
+    }
+
+    toString() {
+        return this.theString;
+    }
+
+}
