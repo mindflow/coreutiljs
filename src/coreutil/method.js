ca901a3496e9cd5215fbd69300bee86d15134fd2
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2025-12-20 23:50:00 +1100

    Refactor Method.

--- src/coreutil/method.js
+++ src/coreutil/method.js
@@ -9,31 +9,32 @@ export class Method{
 
     /**
      * Contructor
-     * @param {T} theObject 
      * @param {F} theFunction 
+     * @param {T} theObject 
      */
-    constructor(theObject, theFunction){
+    constructor(theFunction, theObject = null){
 
         /** @type {T} */
         this.object = theObject;
 
-        /** @type {F} */
+        /** @type {function} */
         this.function = theFunction;
-        if (this.function === null || this.function === undefined) {
+        if (this.function === null || this.function === undefined || typeof this.function !== 'function') {
             console.trace();
             throw new Error("Method function missing");
         }
     }
 
     /**
-     * Calls the function and passed the object as first parameter, and the provided paramter as the second paramter
+     * Calls the function and passed the object as first parameter,  and the provided paramter as the second paramter
      * @param {any} param
      */
     call(param){
+        const targetObject = this.object ? this.object : this;
         if (Array.isArray(param)) {
-            return this.function.call(this.object, ...param);
+            return this.function.call(targetObject, ...param);
         }
-        return this.function.call(this.object, param);
+        return this.function.call(targetObject, param);
     }
 
 }
