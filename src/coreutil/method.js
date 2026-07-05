7b79e9e2cfa5f71ade12104f73d689953cf40569
Toby Nilsen <torbjorn.nilsen@gmail.com>
2026-07-05 19:39:14 +1000

    optional explode paramter on method.

--- src/coreutil/method.js
+++ src/coreutil/method.js
@@ -11,11 +11,15 @@ export class Method{
      * Contructor
      * @param {F} theFunction 
      * @param {T} theObject 
+     * @param {boolean} explodeArrayParam
      */
-    constructor(theFunction, theObject = null){
+    constructor(theFunction, theObject = null, explodeArrayParam = true){
 
         /** @type {T} */
         this.object = theObject;
+
+        /** @type {boolean} */
+        this.explodeArrayParam = explodeArrayParam;
 
         /** @type {function} */
         this.function = theFunction;
@@ -31,7 +35,7 @@ export class Method{
      */
     call(param){
         const targetObject = this.object ? this.object : this;
-        if (Array.isArray(param)) {
+        if (Array.isArray(param) && this.explodeArrayParam) {
             return this.function.call(targetObject, ...param);
         }
         return this.function.call(targetObject, param);
